9fbb67a00f0500423d5da689679c999fffd1e751
Grant Miller <GrantM11235@gmail.com>
2017-08-01 00:07:12 -0500

    Add DSO138 to merge_sketch.bat

--- sketch_and_bootloader_combined/merge_sketch.bat
+++ sketch_and_bootloader_combined/merge_sketch.bat
@@ -9,6 +9,7 @@ for /f "delims=" %%A in ('forfiles /p ..\STM32F1\binaries /s /m generic*.bin /c 
 
 .\bin_merge_tool\mergesketch.exe ..\STM32F1\binaries\naze32_boot20.bin .\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\merged_bins\naze32_boot20.bin
 .\bin_merge_tool\mergesketch.exe ..\STM32F1\binaries\stbee_boot20.bin  .\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\merged_bins\stbee_boot20.bin
+.\bin_merge_tool\mergesketch.exe ..\STM32F1\binaries\dso138_boot20.bin  .\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\merged_bins\dso138_boot20.bin
 
 setlocal disableDelayedExpansion
 for /f "delims=" %%A in ('forfiles /p ..\STM32F1\binaries /s /m maple*.bin /c "cmd /c echo @relpath"') do (
