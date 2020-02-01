9e18f90cbd0a6e5b3459bf5cb8928dc60baf3c94
Roger Clark <roger@rogerclark.net>
2020-02-02 10:11:04 +1100

    Updated bat files to include or handle new bootloader makefile targets etc

--- merge_sketch_and_bootloaders.bat
+++ merge_sketch_and_bootloaders.bat
@@ -1,15 +1,13 @@
 '@echo off
 setlocal disableDelayedExpansion
-for /f "delims=" %%A in ('forfiles /p .\bootloader_only_binaries /s /m generic*.bin /c "cmd /c echo @relpath"') do (
+for /f "delims=" %%A in ('forfiles /p .\bootloader_only_binaries /s /m *.bin /c "cmd /c echo @relpath"') do (
   set "file=%%~A"
   setlocal enableDelayedExpansion
   .\sketch_combiner\bin_merge_tool\mergesketch.exe .\bootloader_only_binaries\!file:~2! .\sketch_combiner\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\binaries\!file:~2!
   endlocal
 )
 
-.\sketch_combiner\bin_merge_tool\mergesketch.exe .\bootloader_only_binaries\naze32_boot20.bin .\sketch_combiner\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\binaries\naze32_boot20.bin
-.\sketch_combiner\bin_merge_tool\mergesketch.exe .\bootloader_only_binaries\stbee_boot20.bin  .\sketch_combiner\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\binaries\stbee_boot20.bin
-.\sketch_combiner\bin_merge_tool\mergesketch.exe .\bootloader_only_binaries\dso138_boot20.bin  .\sketch_combiner\congratulations_sketch\congratulations_sketch.ino.generic_stm32f103c.bin .\binaries\dso138_boot20.bin
+
 
 setlocal disableDelayedExpansion
 for /f "delims=" %%A in ('forfiles /p .\bootloader_only_binaries /s /m maple*.bin /c "cmd /c echo @relpath"') do (
